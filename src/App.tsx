91a9dc2f7ff16bbd994d6749c081b283e77daf5b
maksim.samorukov <maxim.samorukov@gmail.com>
2025-12-18 16:21:44 +0300

    init_1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div>
-      fsdfsf
+      fsdfsdfghdfghffgfg
     </div>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.module.scss';
+
+export const Header = () => {
+    return (
+        <div className={styles.container}>
+            Header
+        </div>
+    );
+};
